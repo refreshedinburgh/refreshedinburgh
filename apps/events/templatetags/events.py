a32f09f6a5bce78bec70e63fe51ace89c50ba78f
flother <matt.riggott@gmail.com>
2009-02-18 22:50:35 +0000

    Added a template tag to allow the three most recent events to be displayed on the home page.

---
+++ apps/events/templatetags/events.py
@@ -0,0 +1,16 @@
+from django.template import Library, Node
+from apps.events.models import Event
+
+
+register = Library()
+
+
+class LatestEventsNode(Node):
+    def render(self, context):
+        context['event_list'] = Event.objects.all()[:3]
+        return ''
+
+
+@register.tag
+def get_latest_events(parser, token):
+    return LatestEventsNode()
