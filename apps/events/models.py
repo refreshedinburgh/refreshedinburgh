a539db8712a74ba9fa1115b4fda1902c6b1295ee
flother <matt.riggott@gmail.com>
2009-02-18 21:25:59 +0000

    Completed the Event and Location models and finished off their admin models too.  URLs have been added with incomplete views too.

--- apps/events/models.py
+++ apps/events/models.py
@@ -4,14 +4,34 @@ from django.db import models
 
 
 class Location(models.Model):
-    title = models.CharField(max_length=60)
-    lat = models.DecimalField(max_digits=10, decimal_places=8)
-    lng = models.DecimalField(max_digits=10, decimal_places=8)
-    link = models.URLField()
+    name = models.CharField(max_length=60)
+    slug = models.SlugField()
+    street = models.CharField(max_length=128, blank=True)
+    post_code = models.CharField(max_length=8, blank=True)
+    latitude = models.DecimalField(max_digits=10, decimal_places=8, blank=True,
+        null=True)
+    longitude = models.DecimalField(max_digits=10, decimal_places=8,
+        blank=True, null=True)
+    url = models.URLField('URL', blank=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __unicode__(self):
+        return self.name
 
 
 class Event(models.Model):
+    title = models.CharField(max_length=64)
+    slug = models.SlugField(unique_for_year=True)
+    date = models.DateTimeField(unique=True)
     location = models.ForeignKey(Location)
-    dtstart = models.DateTimeField()
     copy = models.TextField()
-    upcoming_link = models.URLField()
+    upcoming_link = models.URLField(blank=True)
+
+    class Meta:
+        get_latest_by = 'date'
+        ordering = ('-date',)
+
+    def __unicode__(self):
+        return self.title
--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import handler404, handler500, include, patterns
+from django.conf import settings
 from django.contrib import admin
 
 import settings
@@ -8,9 +9,9 @@ admin.autodiscover()
 
 
 urlpatterns = patterns('',
+    (r'^', include('apps.core.urls')),
     (r'^events/', include('apps.events.urls')),
     (r'^admin/', include(admin.site.urls)),
-    (r'^', include('apps.core.urls')),
 )
 
 
@@ -19,3 +20,4 @@ if settings.SERVER_ENV == 'development':
         (r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:], 'django.views.static.serve',
             {'document_root': settings.MEDIA_ROOT, 'show_indexes': True})
     )
+
